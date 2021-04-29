38715884d6190c5fd335ab555b7e36041d4ea267
John Chen <tttom3669@gmail.com>
2021-04-29 17:31:13 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,13 @@
+package com.tom
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human(){
+    fun  hello(){
+        println("Hello Kotlin")
+    }
+}
